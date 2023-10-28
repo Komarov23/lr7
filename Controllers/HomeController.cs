3849cd12ddb8331420f1dc7a7f3eab7b9668f2aa
Komarov23 <komarov2004qw@gmail.com>
2023-10-28 17:44:52 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lr7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            System.IO.File.WriteAllText($"App_Data/{fileName}.txt", $"Ім'я: {firstName}\nПрізвище: {lastName}");
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), $"App_Data/{fileName}.txt"), "text/plain", $"{fileName}.txt");
+        }
+    }
+}
